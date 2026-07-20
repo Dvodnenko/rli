49cfbb1a8d76ea168b4ee7cabdc8f3fa4676576b
Dvodnenko <thekovalskii@gmail.com>
2026-07-20 16:25:14 +0300

    add 2nd graph
    it shows how epsilon value changes the learning process

--- src/nab/main.py
+++ src/nab/main.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 10 # n of arms
-epsilon = 0.01
 
-def select_action(Qs) -> int:
+
+def select_action(Qs, epsilon: float) -> int:
     """return index of a greedy action or an exploratory action"""
     if random.random() <= epsilon:
         return random.randint(0, n-1) # random action
@@ -24,14 +24,14 @@ def average(a: int, reward: float, Qs, N):
     Qs[a] += 1/N[a] * (reward - Qs[a])
 
 
-def run(n_steps=1000):
+def run(n_steps=1000, epsilon: float = 0):
     qs = np.random.normal(0, 1, size=n)
     Qs = np.zeros(n)
     N = np.zeros(n)
     rewards = np.zeros(n_steps)
     
     for s in range(n_steps):
-        a = select_action(Qs)
+        a = select_action(Qs, epsilon)
         reward = pull(a, qs)
         count(a, N)
         average(a, reward, Qs, N)
@@ -40,15 +40,16 @@ def run(n_steps=1000):
     return rewards
 
 
-def experiment(n_runs=2000, n_steps=1000):
+def experiment(n_runs=3000, n_steps=1000, epsilon: float = 0):
     all_rewards = np.zeros((n_runs, n_steps))
     
     for r in range(n_runs):
-        all_rewards[r] = run(n_steps)
+        all_rewards[r] = run(n_steps, epsilon)
     
     avg_rewards = all_rewards.mean(axis=0)  # average across runs, per timestep
     return avg_rewards
 
 
-plt.plot(experiment())
+plt.plot(experiment(epsilon=0.1))
+plt.plot(experiment(epsilon=0.01))
 plt.show()
