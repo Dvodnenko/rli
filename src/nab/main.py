5bbc601a359e15db661a3101d112476b905db2b0
Dvodnenko <thekovalskii@gmail.com>
2026-07-21 13:14:47 +0300

    refactor nab

--- src/nab/main.py
+++ src/nab/main.py
@@ -1,55 +1,69 @@
 import random
+from dataclasses import dataclass
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10 # n of arms
+
+@dataclass(frozen=True)
+class Bandit:
+    n: int # n of arms
+    qs: np.ndarray # actual values of arms
+
+    def pull(self, arm: int) -> float:
+        return np.random.normal(self.qs[arm], 1)
 
 
-def select_action(Qs, epsilon: float) -> int:
-    """return index of a greedy action or an exploratory action"""
-    if random.random() <= epsilon:
-        return random.randint(0, n-1) # random action
-    return Qs.argmax() # greedy action
+@dataclass
+class Agent:
+    bandit: Bandit
+    Qs: np.ndarray
+    N: np.ndarray
+    epsilon: float = 0 # greedy by default
 
-def count(a: int, N):
-    N[a] += 1
-    return
+    def select_action(self):
+        """return index of a greedy action or an exploratory action"""
+        if random.random() <= self.epsilon:
+            return random.randint(0, self.bandit.n-1) # random action
+        return self.Qs.argmax() # greedy action
 
-def pull(a: int, qs) -> float:
-    """simulate pulling arm a, returns a noisy reward"""
-    return np.random.normal(qs[a], 1)
+    def update(self, arm: int, reward: float):
+        self.N[arm] += 1
+        self.Qs[arm] += 1/self.N[arm] * (reward - self.Qs[arm])
 
-def average(a: int, reward: float, Qs, N):
-    Qs[a] += 1/N[a] * (reward - Qs[a])
+    def step(self) -> float:
+        """Agent's whole action-reward cycle"""
+        action = self.select_action()
+        reward = self.bandit.pull(action)
+        self.update(action, reward)
+        return reward
 
 
-def run(n_steps=1000, epsilon: float = 0):
+def run(n_steps: int, n: int, epsilon: float):
     qs = np.random.normal(0, 1, size=n)
     Qs = np.zeros(n)
     N = np.zeros(n)
     rewards = np.zeros(n_steps)
-    
+
+    bandit = Bandit(n, qs)
+    agent = Agent(bandit, Qs, N, epsilon)
+
     for s in range(n_steps):
-        a = select_action(Qs, epsilon)
-        reward = pull(a, qs)
-        count(a, N)
-        average(a, reward, Qs, N)
-        rewards[s] = reward
-    
+        rewards[s] = agent.step()
     return rewards
 
 
-def experiment(n_runs=3000, n_steps=1000, epsilon: float = 0):
+def experiment(n_runs=2000, n_steps=1000, n: int= 10, epsilon: float = 0):
     all_rewards = np.zeros((n_runs, n_steps))
     
     for r in range(n_runs):
-        all_rewards[r] = run(n_steps, epsilon)
+        all_rewards[r] = run(n_steps, n, epsilon)
     
     avg_rewards = all_rewards.mean(axis=0)  # average across runs, per timestep
     return avg_rewards
 
 
-plt.plot(experiment(epsilon=0.1))
-plt.plot(experiment(epsilon=0.01))
+plt.plot(experiment(n=10, epsilon=0.001))
+plt.xlabel("Steps")
+plt.ylabel("Average reward")
 plt.show()
