e78bf85340a031040478e1803a2c4e9f92e39ea4
Dvodnenko <thekovalskii@gmail.com>
2026-07-30 17:06:25 +0300

    add experiment context

---
+++ src/nab/context.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, kw_only=True)
+class ExperimentContext:
+
+    n_runs: int = 2000 # number of runs in the experiment
+    n_steps: int = 1000 # number of steps in each run
+
+    n_arms: int = 10 # number of arms
+    drift_std: float = 0 # how much each arm's value drifts per step (for NSP)
+
+    alpha: float | None = None # constant step-size parameter
+    epsilon: float = 0 # randomness probability; greedy by default
+    optimism: float = 0 # regulates initial optimism; realistic by default
+
+    def __post_init__(self):
+        """Validation of parameters"""
+        if min(self.n_runs, self.n_steps, self.n_arms) <= 0:
+            raise ValueError("N of runs, steps or arms can't be <= 0")
+
+        if self.alpha is not None:
+            if not (0 < self.alpha <= 1):
+                raise ValueError("Alpha must be in range (0, 1]")
+
+        if not (0 <= self.epsilon <= 1):
+            raise ValueError("Epsilon must be in range [0, 1]")
